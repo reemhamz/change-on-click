5d559af02ab244c668b937993514149f125baadc
Reem H <reemhamoui92@gmail.com>
2020-01-30 12:01:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FlipImage extends Component {
+
+  constructor() {
+    super();
+
+    //setting state to a variable holding a boolean. 
+    //This boolean will change to false and back to true whenever the image is clicked.
+    this.state = {
+      flipTheImage: true,
+    }
+  }
+
+//when image is clicked, the boolean above changes to either true or false
+  clicked = () => {
+    this.setState({
+      flipTheImage: !this.state.flipTheImage
+    })
+  }
+  
+  //made a function holding an if-else statement that would return a string similar
+  //to the image paths in the imagesToFlip variable below in the render method
+  changeImagePath = () => {
+    if (this.state.flipTheImage === true) {
+      return 'a'
+    } else {
+      return 'b'
+    }
+  }
+  
+  render() {
+    
+    const imagesToFlip = {
+      a: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Cat_poster_1.jpg",
+      b: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg"
+    }
+
+    const imagePath = this.changeImagePath();
+
+        return (
+            <div className="FlipImage">
+            <img className="image" src={imagesToFlip[imagePath]} alt="Collage of animals" onClick={this.clicked}/>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default FlipImage;
